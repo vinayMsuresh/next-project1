fe6e8708c8b748c9392081495a51d60c8886999e
jayvsajay <jayvsajay734@gmail.com>
2022-07-04 16:21:53 +0530

    adminlogin

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
-import { Button, Typography } from '@mui/material'
+import { Button, Typography,Grid } from '@mui/material'
 import styles from '../styles/Home.module.css'
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
+import Image from 'next/image';
 
+const styles1=theme=>({
+  TextField:{
+    color:'white'
+  },
+  input: {
+    color: 'white'
+}
+})
 export default function Home() {
   const [email,setEmail]=useState();
   const [password,setPassword]=useState();
   return (
+    <Grid container direction='row' >
+      <Grid item md={6} sx={{mt:5,p:5}}>
+        <Image src='/login.gif' alt="imad" width="500px" height='400px' />
+
+      </Grid>
+      <Grid item md={6} >
     <div className={styles.container}>
   
       <Stack
       component="form"
       sx={{
         width: '25ch',
-        margin:"auto"
-        
+        margin:"auto",
       }}
       spacing={2}
       noValidate
@@ -28,15 +42,19 @@ export default function Home() {
        type="email"
         size="small"
         value={email}
+        className={styles1.TextField}
         name='email'
         onChange={(e) => setEmail(e.target.value)}
         error={email === ""}
         helperText={email === "" ? 'Empty field!' : ' '}
+        InputProps={{
+          className: styles1.input,
+      }}
       />
       <TextField
-      error
       name="password"
         type="password"
+        
         id="filled-hidden-label-normal"
         placeholder='password'
         value={password}
@@ -47,5 +65,7 @@ export default function Home() {
     </Stack>
     
     </div>
+    </Grid>
+    </Grid>
   )
 }
