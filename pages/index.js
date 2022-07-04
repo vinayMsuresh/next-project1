827cd0c15b7d5854267537d8b23fcafbf6081d8f
jayvsajay <jayvsajay734@gmail.com>
2022-07-04 11:59:02 +0530

    Added admin ui fields

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,39 @@
+import { Typography } from '@mui/material'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h2>Home page</h2>
+      <Typography variant='h2'>Admin Login</Typography>
+      <Stack
+      component="form"
+      sx={{
+        width: '25ch',
+        
+      }}
+      spacing={2}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        hiddenLabel
+        id="filled-hidden-label-small"
+        placeholder='Enter Email id'
+       type="email"
+        size="small"
+      />
+      <TextField
+      type="password"
+        hiddenLabel
+        id="filled-hidden-label-normal"
+        placeholder='password'
+       
+      />
+    </Stack>
     </div>
   )
 }
