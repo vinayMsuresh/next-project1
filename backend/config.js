dc3e08c49e07cd08732fce768e218331c48356aa
jayvsajay <jayvsajay734@gmail.com>
2022-07-07 16:17:54 +0530

    Changes made

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,4 @@
 module.exports={
-    clientId:'132461580653-bds4mpp6grlsv4bcct70l7hr2s2appvh.apps.googleusercontent.com',
-    clientScret:'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',
-    refreshToken:'1//04BAeAqyM1HgHCgYIARAAGAQSNwF-L9IrGP4yGq5tVxy_HfduAcmUbG0qwawv-cBdfLqb2Eau7i2hnvbCzO7Wd_u6-7GRQDA2sLw',
-    user:'jayvsajay734@gmail.com'
+   
 
 }
